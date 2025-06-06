fb45dd9419d97b68319d7e95212c28dd8de39248
Yash <yashrana21032006@gmail.com>
2025-06-06 16:33:22 +0530

    majority element

---
+++ src/leetcode_150/Easy_150_5_majority_element.java
@@ -0,0 +1,46 @@
+//https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+package leetcode_150;
+
+public class Easy_150_5_majority_element {
+    // using Boyer-Moore Majority Vote Algorithm
+    public static int optimal(int[] nums) {
+        int n = nums.length;
+        int votes = 0, candidate = -1;
+        for (int ele: nums) {
+            if (votes == 0) candidate = ele;
+            votes += (ele == candidate) ? 1 : -1;
+        }
+        
+        int count = 0;
+        for (int ele : nums) if (ele == candidate) count++;
+
+        return (count >= n / 2) ? candidate : -1;
+
+        // TC: O(N)
+        // SC: O(1)
+    }
+
+    // same but chatGPT
+    public static int chatGPT(int[] nums){
+        int votes = 0, candidate = -1;
+        for (int ele: nums) {
+            if(votes == 0) candidate = ele;
+            votes += (ele == candidate) ? 1 : -1;
+        }
+        return candidate;
+
+        // TC: O(N)
+        // SC: O(1)
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{3, 2, 3};
+//        int[] nums = new int[]{2, 2, 1, 1, 1, 2, 2};
+//        int[] nums = new int[]{1, 1, 1, 1, 2, 3, 4};
+        int[] nums = new int[]{9, 9, 9, 5, 6, 9, 7, 9, 8};
+
+        System.out.println(optimal(nums));
+        System.out.println(chatGPT(nums));
+    }
+}
