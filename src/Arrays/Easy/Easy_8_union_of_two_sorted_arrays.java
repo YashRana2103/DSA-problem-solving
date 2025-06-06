a2c175a62c9089533fc2f446b999fdb2bf2f2e62
Yash <yashrana21032006@gmail.com>
2025-06-06 11:49:06 +0530

    union of two sorted array. Changes in PrintArr's methods.

---
+++ src/Arrays/Easy/Easy_8_union_of_two_sorted_arrays.java
@@ -0,0 +1,77 @@
+package Arrays.Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static mypackage.PrintArr.printArr;
+
+public class Easy_8_union_of_two_sorted_arrays {
+    public static int[] optimal(int[] arr1, int n, int[] arr2, int m) {
+        int[] arr3 = new int[n + m];
+        int i = 0, j = 0, k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                if (k == 0 || arr3[k - 1] != arr1[i]) arr3[k++] = arr1[i];
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (k == 0 || arr3[k - 1] != arr2[j]) arr3[k++] = arr2[j];
+                j++;
+            } else {
+                if (k == 0 || arr3[k - 1] != arr1[i]) arr3[k++] = arr1[i];
+                i++;
+                j++;
+            }
+        }
+        while (i < n) {
+            if (k == 0 || arr3[k - 1] != arr1[i]) arr3[k++] = arr1[i];
+            i++;
+        }
+        while (j < m) {
+            if (k == 0 || arr3[k - 1] != arr2[j]) arr3[k++] = arr2[j];
+            j++;
+        }
+
+        return Arrays.copyOfRange(arr3, 0, k);
+    }
+
+    public static ArrayList<Integer> tuf(int[] arr1, int n, int[] arr2, int m) {
+        int i = 0, j = 0;
+
+        ArrayList<Integer> unionArr = new ArrayList<>();
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                if (unionArr.isEmpty() || unionArr.getLast() != arr1[i]) unionArr.add(arr1[i]);
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (unionArr.isEmpty() || unionArr.getLast() != arr2[j]) unionArr.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < n) {
+            if (unionArr.isEmpty() || unionArr.getLast() != arr1[i]) unionArr.add(arr1[i]);
+            i++;
+        }
+        while (j < m) {
+            if (unionArr.isEmpty() || unionArr.getLast() != arr2[j]) unionArr.add(arr2[j]);
+            j++;
+        }
+        return unionArr;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr1 = new int[]{1, 2, 3, 4, 5};
+//        int[] arr2 = new int[]{2, 3, 4, 4, 5};
+//        Output: {1, 2, 3, 4, 5}
+
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = new int[]{2, 3, 4, 4, 5, 11, 12};
+//        Output: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+
+        int[] arr = optimal(arr1, arr1.length, arr2, arr2.length);
+        printArr(arr);
+
+//        ArrayList<Integer> unionArr = new ArrayList<>(tuf(arr1, arr1.length, arr2, arr2.length));
+//        System.out.println(unionArr);
+    }
+}
--- src/mypackage/PrintArr.java
+++ src/mypackage/PrintArr.java
@@ -1,17 +1,27 @@
 package mypackage;
 
 public class PrintArr {
+    // Prints the entire array in [1, 2, 3] format
     public static void printArr(int[] arr) {
+        System.out.print("[");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
         }
-        System.out.println();
+        System.out.println("]");
     }
 
+    // Prints first n elements in [1, 2, 3] format
     public static void printArr(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+        System.out.print("[");
+        for (int i = 0; i < n && i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != n - 1 && i != arr.length - 1) {
+                System.out.print(", ");
+            }
         }
-        System.out.println();
+        System.out.println("]");
     }
 }
